2ab28ab93766d6b0e9532fb410551e54eb0abceb
n.pistsov <n.pistsov@ritm.media>
2023-09-13 22:56:59 +0300

    minor cosmitic changes

--- main.py
+++ main.py
@@ -18,12 +18,13 @@ def start(message):
     markup.add(btn1)
     markup.add(btn2)
     markup.add(btn3)
-    bot.send_message(message.from_user.id, 'Привет! Я умею выводить курс евро и доллара! Жми кнопку', reply_markup=markup)
+    bot.send_message(message.from_user.id, 'Привет! Я умею выводить курс евро и доллара! Жми кнопку. '
+                                           'На другие сообщения я отвечать не стану', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def send_message(message):
-    print(message.from_user.username, message.text)
+    print(message.chat.id, message.from_user.username, message.text)
     if message.text == '🇪🇺Курс евро💶':
         bot.send_message(chat_id=message.chat.id,
                          text='1 евро = ' + str(data['Valute']['EUR']['Value']) + ' рублей по курсу ЦБ РФ на сегодня')
@@ -32,10 +33,11 @@ def send_message(message):
                          text='1 доллар = ' + str(data['Valute']['USD']['Value']) + ' рублей по курсу ЦБ РФ на сегодня')
     elif message.text == '❤️':
         bot.send_message(chat_id=message.chat.id,
-                         text= '@' + message.from_user.username + ', ты просто прелесть!😘💋')
+                         text='@' + message.from_user.username + ', ты просто прелесть!😘💋')
     elif '@pistsovbot' in message.text.lower():
         bot.send_message(chat_id=message.chat.id,
-                         text='Чего это Вы в меня тыкаете? В кнопку тыкай, ' + message.from_user.username + '!')
+                         text='@' + message.from_user.username + ', я тебе русским по белому сказал - только кнопки! '
+                                                           'На китайском ещё сказать что ле? Я пока так не умею...')
 
 
 bot.polling(none_stop=True)
